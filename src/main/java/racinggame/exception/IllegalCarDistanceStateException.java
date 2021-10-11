3d6b3821a6e29e5058756fae22ceb12ece7608a2
wook <mdir2@naver.com>
2021-10-11 21:47:42 +0900

    refactor(exception): argument 관련된 예외 IllegalRacingGameArgumentException 을 상속받도록 변경

--- src/main/java/racinggame/exception/IllegalCarDistanceStateException.java
+++ src/main/java/racinggame/exception/IllegalCarDistanceStateException.java
@@ -1,6 +1,6 @@
 package racinggame.exception;
 
-public class IllegalCarDistanceStateException extends RacingGameRuntimeException {
+public class IllegalCarDistanceStateException extends IllegalRacingGameArgumentException {
     private static final String MESSAGE = "이동 할 수 없는 상태입니다. 생성 된 숫자에 문제가 있습니다.";
 
     public IllegalCarDistanceStateException() {
